49ff74bdd8b405143edb4464a7eee4c748b70bf2
toriiiii <torrance.anonuevo@gmail.com>
2025-11-20 20:48:14 -0500

    register with admin

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import StockBotUser
 
-# Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'botID')
+
+admin.site.register(StockBotUser, UserAdmin)
