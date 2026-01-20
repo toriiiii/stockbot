7d330fe28d19b535fad8e3b61767d59f222fe233
toriiiii <torrance.anonuevo@gmail.com>
2026-01-19 22:49:42 -0500

    allow local envvars to be used

--- backend/stockbot_backend/settings.py
+++ backend/stockbot_backend/settings.py
@@ -14,6 +14,10 @@ from pathlib import Path
 import dj_database_url
 import os
 
+# Load environment variables for local development
+from dotenv import load_dotenv
+load_dotenv() 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
