a033ad5a4812a762de4a21e5b7f6efeeb0d81b56
toriiiii <torrance.anonuevo@gmail.com>
2026-01-22 01:00:48 -0500

    add render.yaml and remove debug

--- backend/stockbot_backend/settings.py
+++ backend/stockbot_backend/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%p!6@lnrc&e%wn#mz(*gmwxra-!on3swex^j5hu-t9^bg)o+%j'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
