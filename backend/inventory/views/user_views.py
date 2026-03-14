68cf3c84f45e80d9cf402dad6ecc620a799920ae
toriiiii <torrance.anonuevo@gmail.com>
2026-03-14 18:08:52 -0400

    add auth backend config

--- backend/inventory/views/user_views.py
+++ backend/inventory/views/user_views.py
@@ -1,36 +1,24 @@
 from rest_framework import generics, permissions
 from ..models import Item
 from ..serializers import ItemSerializer
-from django.contrib.auth import get_user_model
 
-### ALL ENDPOINTS ARE PUBLIC DURING DEVELOPMENT
-
-User = get_user_model()
 class ItemListCreateView(generics.ListCreateAPIView):
-    """Public GET + POST for dev/testing"""
     serializer_class = ItemSerializer
-    permission_classes = [permissions.AllowAny]
+    permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
-        #TO DO: Limit objects to authorized user
-        return Item.objects.all()
+        return Item.objects.filter(user=self.request.user)
 
     def perform_create(self, serializer):
-        #TO DO: Get user by auth
-        first_user = User.objects.order_by('id').first()
-        serializer.save(user=first_user)
+        serializer.save(user=self.request.user)
 
 
 class ItemDetailView(generics.RetrieveUpdateDestroyAPIView):
-    """Public GET, PUT, PATCH, DELETE for dev/testing"""
     serializer_class = ItemSerializer
-    permission_classes = [permissions.AllowAny]
+    permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
-        #TO DO: Limit objects to authorized user
-        return Item.objects.all()
+        return Item.objects.filter(user=self.request.user)
 
     def perform_update(self, serializer):
-        #TO DO: Get user by auth
-        first_user = User.objects.order_by('id').first()
-        serializer.save(user=first_user)
+        serializer.save(user=self.request.user)
