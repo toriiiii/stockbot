4f84318e5cc60892337b8a03479c76cd398cef8e
toriiiii <torrance.anonuevo@gmail.com>
2026-03-16 21:27:14 -0400

    add bot_id unique validation

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class StockBotUser(AbstractUser):
-    botID = models.IntegerField(default=0)
+    botID = models.IntegerField(default=0, unique=True)
 
     def __str__(self):
         return self.username
