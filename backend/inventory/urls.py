9f258e1ae20ed65205cdfe373d177333fbf8b093
toriiiii <torrance.anonuevo@gmail.com>
2026-03-19 18:30:55 -0400

    ingest image from ai server

--- backend/inventory/urls.py
+++ backend/inventory/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
 from .views import ItemListCreateView, ItemDetailView
-from .views import CameraIngestionView, ClassificationIngestionView, FSRIngestionView
+from .views import ClassificationIngestionView, FSRIngestionView
 
 urlpatterns = [
     path("items/", ItemListCreateView.as_view(), name="item-list"),
     path("items/<int:pk>/", ItemDetailView.as_view(), name="item-detail"),
 
-    path("ingestion/camera/", CameraIngestionView.as_view(), name="ingestion-camera"),
     path("ingestion/classification/", ClassificationIngestionView.as_view(), name="ingestion-classification",),
     path("ingestion/fsr/", FSRIngestionView.as_view(), name="ingestion-fsr"),
 ]
--- backend/inventory/views/sensor_views.py
+++ backend/inventory/views/sensor_views.py
@@ -3,51 +3,12 @@ from rest_framework.response import Response
 
 from accounts.models import StockBotUser
 from inventory.models import SensorIngestionEvent
-from ..serializers import CameraIngestionSerializer, ClassificationIngestionSerializer, FSRIngestionSerializer
+from ..serializers import ClassificationIngestionSerializer, FSRIngestionSerializer
 from inventory.services.ingestion import try_resolve_event
 
 import logging
 
 logger = logging.getLogger(__name__)
-class CameraIngestionView(generics.CreateAPIView):
-    serializer_class = CameraIngestionSerializer
-    permission_classes = [permissions.AllowAny]
-
-    def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-
-        data = serializer.validated_data
-        try:
-            user = StockBotUser.objects.get(botID=data["bot_id"])
-        except StockBotUser.DoesNotExist:
-            logger.error(f'Camera update failed — bot id user not found')
-            return Response(
-                {"error": "Invalid bot_id"},
-                status=status.HTTP_400_BAD_REQUEST
-            )
-
-        event, _ = SensorIngestionEvent.objects.get_or_create(
-            user=user,
-            image_id=data["image_id"],
-        )
-
-        if "image" in data:
-            event.image = data["image"]
-            event.save()
-
-        resolved = try_resolve_event(event)
-        if resolved:
-            logger.info(f'Sensor ingestion event resolved')
-
-        return Response(
-            {
-                "status": "accepted",
-                "image_id": data["image_id"],
-                "resolved": resolved,
-            },
-            status=status.HTTP_202_ACCEPTED,
-        )
 
 class ClassificationIngestionView(generics.CreateAPIView):
     serializer_class = ClassificationIngestionSerializer
@@ -64,7 +25,7 @@ class ClassificationIngestionView(generics.CreateAPIView):
         try:
             user = StockBotUser.objects.get(botID=data["bot_id"])
         except StockBotUser.DoesNotExist:
-            logger.error(f'Camera update failed — bot id user not found')
+            logger.error(f'Classification update failed — bot id user not found')
             return Response(
                 {"error": "Invalid bot_id"},
                 status=status.HTTP_400_BAD_REQUEST
@@ -76,11 +37,14 @@ class ClassificationIngestionView(generics.CreateAPIView):
         )
 
         event.classification = data["classification"]
-        event.expires_at = data["expires_at"]
+        event.expires_at = data.get("expires_at")
         event.save()
 
+        event.refresh_from_db() 
+        image_file = data.get("image") 
+
         event.refresh_from_db()
-        resolved = try_resolve_event(event)
+        resolved = try_resolve_event(event,image=image_file)
 
         logger.info(f'Classification data processed — bot_id={data["bot_id"]} image_id={data["image_id"]}')
 
