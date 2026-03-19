e7f52fdebde48af85d015cf449c1d9411334109d
toriiiii <torrance.anonuevo@gmail.com>
2026-03-19 19:45:47 -0400

    try django serve

--- backend/stockbot_backend/urls.py
+++ backend/stockbot_backend/urls.py
@@ -1,10 +1,14 @@
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+import re
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/auth/", include("accounts.urls")),
     path("api/inventory/", include("inventory.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+] 
